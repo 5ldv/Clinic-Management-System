78f61c41dc24495508f5991d2bbcff1a3cc6aaca
Khalid Alshammari <khalidshammari@outlook.sa>
2024-10-10 10:57:26 +0300

    Add connection string to app.config file

--- DataAccess/DataAccessSettings.cs
+++ DataAccess/DataAccessSettings.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Configuration;
 
 namespace ClinicManagement_DataAccess
 {
     static class clsDataAccessSettings
     {
-        public static string ConnectionString = "Data Source=.;Database=ClinicManagement;Integrated Security=True;TrustServerCertificate=True;";
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["ConnectingString"].ToString();
     }
 }
